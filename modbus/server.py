f35acaffd7bb2f646fbfaa348e822b457dd7af5c
WebElektronika <99819339+webelektronika@users.noreply.github.com>
2022-09-28 11:29:29 +0200

    Update server.py

--- modbus/server.py
+++ modbus/server.py
@@ -37,7 +37,7 @@ identity.UserApplicationName = 'Modbus Test'
 FORMAT = ('%(asctime)-15s %(threadName)-15s' '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
 
 #logging.basicConfig(format=FORMAT)
-logging.basicConfig(filename='logModbusPentek.txt', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename='logModbus.txt', encoding='utf-8', level=logging.DEBUG)
 log = logging.getLogger()
 
 #log.setLevel(logging.INFO)
