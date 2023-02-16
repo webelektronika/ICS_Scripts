888dba9d635e3d7bc0f558cef529b1e4da75b505
WebElektronika <99819339+webelektronika@users.noreply.github.com>
2023-02-16 16:03:13 +0100

    Update portscan1.py

--- portscan/portscan1.py
+++ portscan/portscan1.py
@@ -13,6 +13,6 @@ def portScanner(port):
     if sock.connect_ex((hostIP, int(port))):
         print(termcolor.colored(("Port %d is closed." % int(port)), "red"))
     else:
-		    print(termcolor.colored(("Port %d is open." % int(port)), "green"))
+        print(termcolor.colored(("Port %d is open." % int(port)), "green"))
 
 portScanner(portNumber)
