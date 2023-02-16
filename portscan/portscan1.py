816490da8a8dd2ed6e82ace458745c8d84a6afbf
WebElektronika <99819339+webelektronika@users.noreply.github.com>
2023-02-16 16:00:14 +0100

    Create portscan1.py

---
+++ portscan/portscan1.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+
+import socket
+import sys
+import termcolor
+
+socket.setdefaulttimeout(1)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+hostIP = sys.argv[1]
+portNumber = sys.argv[2]
+
+def portScanner(port):
+    if sock.connect_ex((hostIP, int(port))):
+        print(termcolor.colored(("Port %d is closed." % int(port)), "red"))
+    else:
+		    print(termcolor.colored(("Port %d is open." % int(port)), "green"))
+
+portScanner(portNumber)
